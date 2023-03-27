9aa80a69f3815090652b120a67e32c2ead8fdbf9
Luisa <luisa.santos042@gmail.com>
2023-03-27 20:55:17 -0300

    make connection to API
    connect to fake api using java.net package

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // get data from api
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        URI address = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
     }
 }
