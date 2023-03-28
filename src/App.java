c6034c7c73f0e82b7fd10999eb749d026ce9aca1
Luisa <luisa.santos042@gmail.com>
2023-03-28 09:52:28 -0300

    add JSON parser
    add JSON parser, change url

--- src/App.java
+++ src/App.java
@@ -3,15 +3,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // get data from api
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI address = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(address).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
+
+        // parse data
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> filmList = parser.parse(body);
+        System.out.println(filmList.size());
     }
 }
